782f6f5625516b00cf2b629385e570a1be46bab2
sosuke115 <sosuken115@gmail.com>
2024-02-17 15:46:21 +0900

    add sharegpt flanv2

--- qlora/qlora.py
+++ qlora/qlora.py
@@ -622,6 +622,14 @@ def make_data_module(tokenizer: transformers.PreTrainedTokenizer, args) -> Dict:
             return load_dataset("studio-ousia/oasst1-instruction")
         elif dataset_name == 'oasst1-89k-ja-instruction':
             return load_dataset("studio-ousia/oasst1-89k-ja-instruction")
+        elif dataset_name == 'sharegpt_0_1':
+            return load_dataset("studio-ousia/sharegpt_0_1")
+        elif dataset_name == 'sharegpt_n-1_n':
+            return load_dataset("studio-ousia/sharegpt_n-1_n")
+        elif dataset_name == 'flan_v2_0_1':
+            return load_dataset("studio-ousia/flan_v2_0_1")
+        elif dataset_name == 'flan_v2_n-1_n':
+            return load_dataset("studio-ousia/flan_v2_n-1_n")
         elif dataset_name == 'alpaca-clean':
             return load_dataset("yahma/alpaca-cleaned")
         elif dataset_name == 'chip2':
